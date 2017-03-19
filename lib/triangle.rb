135280b8c7bbb5c8b37938c46c69285155b434e7
szrharrison <s.z.r.harrison@gmail.com>
2017-03-19 19:00:38 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require 'pry'
 class Triangle
   # write code here
+  def initialize( side_a, side_b, side_c )
+    @sides = []
+    self.sides << side_a
+    self.sides << side_b
+    self.sides << side_c
+  end
+
+  attr_reader :sides
+  ######################################################
+  def kind
+    total = self.sides.reduce(:+)
+    if self.sides.any?{ |side| side <= 0 || total - side <= side}
+      raise TriangleError
+    elsif sides.uniq.length == 1
+      type = :equilateral
+    elsif sides.uniq.length == 2
+      type = :isosceles
+    else
+      type = :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+  def message
+    "The lengths you input cannot be configured into a triangle!"
+  end
 end
